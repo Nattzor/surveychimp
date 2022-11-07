3e3a1889d48b3c02c7630f06ba3761294c8c95c6
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-11-07 10:43:15 +0100

    testing and yaml update

--- deploy/create-environment.sh
+++ deploy/create-environment.sh
@@ -1,4 +1,5 @@
-doctl apps update 59fe9f72-9bd5-46fe-acbf-f5fd02f09be1 --spec ./dev/create-surveychimp-dev.yaml
+doctl apps update bc9defe3-ad90-4ca0-82da-e7230cdbc794 --spec ./dev/create-surveychimp-production.yaml
+# doctl apps update 59fe9f72-9bd5-46fe-acbf-f5fd02f09be1 --spec ./dev/create-surveychimp-dev.yaml
 
 # doctl apps create --spec ./dev/create-surveychimp-production.yaml
 # doctl apps create --spec ./dev/create-surveychimp-dev.yaml
