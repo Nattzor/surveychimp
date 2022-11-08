52cbc3d51f317b65201c46bfd6cac2d1d74cde8d
Markus <markus.mork@iths.se>
2022-11-08 16:01:02 +0100

    Render survey names on separate route

--- apps/surveychimp-survey-spa/src/api/getAllSurvey.ts
+++ apps/surveychimp-survey-spa/src/api/getAllSurvey.ts
@@ -1,6 +1,6 @@
 import { ISurvey } from '@surveychimp/surveychimp-lib';
 
-const getAllSurvey = async (): Promise<ISurvey>  => {
+const getAllSurvey = async (): Promise<ISurvey[]>  => {
     return (await fetch('http://localhost:4200/api/survey')).json();
 }
 
