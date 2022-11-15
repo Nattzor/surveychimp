cc33ef501116c95e192ed69472d8355150e25b59
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-11-15 09:21:37 +0100

    add Sentry

--- apps/surveychimp-survey-spa/src/main.tsx
+++ apps/surveychimp-survey-spa/src/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+Sentry.init({
+    dsn: "https://b1f21ab16bd7465d9a7c149182fa7a42@o4504162132295680.ingest.sentry.io/4504162143240195",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
