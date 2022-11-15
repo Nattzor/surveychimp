5d8e4f4666cbbd165b3ac0f2a259d8dd7ad1cb24
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-11-15 11:01:56 +0100

    test

--- apps/surveychimp-survey-spa/src/main.tsx
+++ apps/surveychimp-survey-spa/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './app/app';
+
 
 Sentry.init({
     dsn: "https://b1f21ab16bd7465d9a7c149182fa7a42@o4504162132295680.ingest.sentry.io/4504162143240195",
