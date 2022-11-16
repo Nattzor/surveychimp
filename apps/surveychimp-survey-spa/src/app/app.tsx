67f56f461fc7da3b0e337585ffc387e5026f4e33
Markus <markus.mork@iths.se>
2022-11-16 11:19:20 +0100

    Install env-cmd to feed env var on dev serve

--- apps/surveychimp-survey-spa/src/app/app.tsx
+++ apps/surveychimp-survey-spa/src/app/app.tsx
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import SurveyPage from './pages/SurveyPage';
 import SurveyPage4star from './pages/SurveyPage4star';
@@ -6,7 +7,7 @@ import AllSurveysPage from './pages/AllSurveysPage';
 import TodoPage from './pages/TodoPage';
 import ThankYouPage from './pages/ThankYou'
 
-import { Route, Routes } from 'react-router-dom';
+
 
 
 
@@ -16,7 +17,7 @@ const StyledApp = styled.div`
 
 
 export function App() {
-
+  console.log('env ', process.env.NX_4STAR_RATING)
   return (
     <StyledApp>
       <Routes>
@@ -25,12 +26,12 @@ export function App() {
           element={<AllSurveysPage />}
         />
         {process.env.NX_4STAR_RATING === "true" ? <Route
-          path="/survey/:surveyId"
-          element={<SurveyPage />}
-        /> : <Route
         path="/survey/:surveyId"
         element={<SurveyPage4star />}
-      /> }
+      /> : <Route
+      path="/survey/:surveyId"
+      element={<SurveyPage />}
+    /> }
 
 
         <Route
